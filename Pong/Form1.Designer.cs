0c8cc3f79262beef87cfde93f16d7915f6d01414
Mr. Theodoropoulos <tony.theodoropoulos@ed.amdsb.ca>
2016-02-03 14:44:53 -0500

    Initial Commit

---
+++ Pong/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace Pong
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.gameUpdateLoop = new System.Windows.Forms.Timer(this.components);
+            this.startLabel = new System.Windows.Forms.Label();
+            this.closeButton = new System.Windows.Forms.Button();
+            this.player1Label = new System.Windows.Forms.Label();
+            this.player2Label = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // gameUpdateLoop
+            // 
+            this.gameUpdateLoop.Interval = 16;
+            this.gameUpdateLoop.Tick += new System.EventHandler(this.gameUpdateLoop_Tick);
+            // 
+            // startLabel
+            // 
+            this.startLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.startLabel.Font = new System.Drawing.Font("Courier New", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startLabel.ForeColor = System.Drawing.Color.White;
+            this.startLabel.Location = new System.Drawing.Point(105, 114);
+            this.startLabel.Name = "startLabel";
+            this.startLabel.Size = new System.Drawing.Size(410, 93);
+            this.startLabel.TabIndex = 0;
+            this.startLabel.Text = "Press Space To Start";
+            this.startLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // closeButton
+            // 
+            this.closeButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.closeButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.closeButton.ForeColor = System.Drawing.Color.White;
+            this.closeButton.Location = new System.Drawing.Point(595, -1);
+            this.closeButton.Margin = new System.Windows.Forms.Padding(0);
+            this.closeButton.Name = "closeButton";
+            this.closeButton.Size = new System.Drawing.Size(21, 24);
+            this.closeButton.TabIndex = 1;
+            this.closeButton.TabStop = false;
+            this.closeButton.Text = "x";
+            this.closeButton.UseVisualStyleBackColor = true;
+            this.closeButton.Click += new System.EventHandler(this.closeButton_Click);
+            // 
+            // player1Label
+            // 
+            this.player1Label.AutoSize = true;
+            this.player1Label.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.player1Label.ForeColor = System.Drawing.Color.White;
+            this.player1Label.Location = new System.Drawing.Point(28, 2);
+            this.player1Label.Name = "player1Label";
+            this.player1Label.Size = new System.Drawing.Size(128, 18);
+            this.player1Label.TabIndex = 2;
+            this.player1Label.Text = "Player 1:  0";
+            // 
+            // player2Label
+            // 
+            this.player2Label.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.player2Label.AutoSize = true;
+            this.player2Label.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.player2Label.ForeColor = System.Drawing.Color.White;
+            this.player2Label.Location = new System.Drawing.Point(430, 2);
+            this.player2Label.Name = "player2Label";
+            this.player2Label.Size = new System.Drawing.Size(128, 18);
+            this.player2Label.TabIndex = 3;
+            this.player2Label.Text = "Player 2:  0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(616, 450);
+            this.Controls.Add(this.player2Label);
+            this.Controls.Add(this.player1Label);
+            this.Controls.Add(this.closeButton);
+            this.Controls.Add(this.startLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Pong";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer gameUpdateLoop;
+        private System.Windows.Forms.Label startLabel;
+        private System.Windows.Forms.Button closeButton;
+        private System.Windows.Forms.Label player1Label;
+        private System.Windows.Forms.Label player2Label;
+    }
+}
+
---
+++ Pong/Form1.cs
@@ -0,0 +1,306 @@
+﻿/*
+ * Description:     A basic PONG simulator
+ * Author:           
+ * Date:            
+ */
+
+#region libraries
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+using System.Media;
+
+#endregion
+
+namespace Pong
+{
+    public partial class Form1 : Form
+    {
+        #region global values
+
+        //paddle position variables
+        int paddle1Y, paddle2Y;
+
+        //ball position variables
+        int ballX, ballY;
+
+        // check to see if a new game can be started
+        Boolean newGameOk = true;
+
+        //ball directions
+        Boolean ballMoveRight = false;
+        Boolean ballMoveDown = false;
+
+        //constants used to set size and speed of paddles 
+        const int PADDLE_LENGTH = 40;
+        const int PADDLE_WIDTH = 10;
+        const int PADDLE_EDGE = 20;  // buffer distance between screen edge and paddle
+        const int PADDLE_SPEED = 4;
+
+        //constants used to set size and speed of ball 
+        const int BALL_SIZE = 10;
+        const int BALL_SPEED = 4;
+
+        //player scores
+        int player1Score = 0;
+        int player2Score = 0;
+
+        //determines whether a key is being pressed or not
+        Boolean aKeyDown, zKeyDown, jKeyDown, mKeyDown;
+
+        //game winning score
+        int gameWinScore = 2;
+
+        #endregion
+       
+        public Form1()
+        {
+            InitializeComponent();
+            SetParameters();        
+        }
+
+        // -- YOU DO NOT NEED TO MAKE CHANGES TO THIS METHOD
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            //check to see if a key is pressed and set is KeyDown value to true if it has
+            switch (e.KeyCode)
+            {
+                case Keys.A:
+                    aKeyDown = true;
+                    break;
+                case Keys.Z:
+                    zKeyDown = true;
+                    break;
+                case Keys.J:
+                    jKeyDown = true;
+                    break;
+                case Keys.M:
+                    mKeyDown = true;
+                    break;
+                case Keys.Y:
+                    if (newGameOk)
+                    {
+                        GameStart();
+                    }
+                    break;
+                case Keys.N:
+                    if (newGameOk)
+                    {
+                        Close();
+                    }
+                    break;
+                case Keys.Space:
+                    if (newGameOk)
+                    {
+                        GameStart();
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+        
+        // -- YOU DO NOT NEED TO MAKE CHANGES TO THIS METHOD
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            //check to see if a key has been released and set its KeyDown value to false if it has
+            switch (e.KeyCode)
+            {
+                case Keys.A:
+                    aKeyDown = false;
+                    break;
+                case Keys.Z:
+                    zKeyDown = false;
+                    break;
+                case Keys.J:
+                    jKeyDown = false;
+                    break;
+                case Keys.M:
+                    mKeyDown = false;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// Sets up the game objects in their start position, resets the scores, displays a 
+        /// countdown, and then starts the game timer.
+        /// </summary>
+        private void GameStart()
+        {
+            newGameOk = true;
+            SetParameters();
+
+            // TODO create code to make a graphics object, a brush, and a font to display the countdown
+
+            startLabel.Visible = false;
+            Refresh();
+            
+            //countdown to start of game
+            for (; ; ) // TODO create conditions for a for loop that counts down from 3 
+            {
+                // --- create code using DrawString to display the countdown in the appropriate area.  
+                // --- sleep for 1 second
+                // --- refresh the screen
+            }
+            
+            // TODO start the gameUpdateLoop timer
+            newGameOk = false;
+        }
+
+        /// <summary>
+        /// sets the ball and paddle positions for game start
+        /// </summary>
+        private void SetParameters()
+        {
+            if (newGameOk)
+            {
+                // sets location of score labels to the middle of the screen
+                player1Label.Location = new Point(this.Width / 2 - player1Label.Size.Width - 10, player1Label.Location.Y);
+                player2Label.Location = new Point(this.Width / 2 + 10, player2Label.Location.Y);
+
+                //set label, score variables, and ball position
+                player1Score = player2Score = 0;
+                player1Label.Text = "Player 1:  " + player1Score;
+                player2Label.Text = "Player 2:  " + player2Score;
+
+                paddle1Y = paddle2Y = this.Height / 2 - PADDLE_LENGTH / 2;
+
+            }
+
+            // TODO set starting X position for ball to middle of screen, (use this.Width and BALL_SIZE)
+            // TODO set starting Y position for ball to middle of screen, (use this.Height and BALL_SIZE)
+
+        }
+
+        /// <summary>
+        /// This method is the game engine loop that updates the position of all elements
+        /// and checks for collisions.
+        /// </summary>
+        private void gameUpdateLoop_Tick(object sender, EventArgs e)
+        {
+            //sound player to be used for all in game sounds initially set to collision sound
+            SoundPlayer player = new SoundPlayer();
+            player = new SoundPlayer(Properties.Resources.collision);
+
+            #region update ball position
+
+            // TODO create code to move ball either left or right based on ballMoveRight and BALL_SPEED
+
+            // TODO create code move ball either down or up based on ballMoveDown and BALL_SPEED
+
+            #endregion
+
+            #region update paddle positions
+
+            if (aKeyDown == true && paddle1Y > 0)
+            {
+                // TODO create code to move player 1 paddle up using paddle1Y and PADDLE_SPEED
+            }
+
+            // TODO create an if statement and code to move player 1 paddle down using paddle1Y and PADDLE_SPEED
+
+            // TODO create an if statement and code to move player 2 paddle up using paddle2Y and PADDLE_SPEED
+
+            // TODO create an if statement and code to move player 2 paddle down using paddle2Y and PADDLE_SPEED
+
+            #endregion
+
+            #region ball collision with top and bottom lines
+
+            if (ballY < 0) // if ball hits top line
+            {
+                // TODO use ballMoveDown boolean to change direction
+                // TODO play a collision sound
+            }
+            // TODO In an else if statement use ballY, this.Height, and BALL_SIZE to check for collision with bottom line
+            // If true use ballMoveDown down boolean to change direction
+
+            #endregion
+
+            #region ball collision with paddles
+
+            if (ballY > paddle1Y && ballY < paddle1Y + PADDLE_LENGTH && ballX < PADDLE_EDGE + PADDLE_WIDTH) // left paddle collision
+            {
+                // TODO play a "paddle hit" sound 
+                // TODO use ballMoveRight boolean to change direction
+            }
+            else if (ballY > paddle2Y && ballY < paddle2Y + PADDLE_LENGTH && ballX + BALL_SIZE > this.Width - PADDLE_EDGE - PADDLE_WIDTH / 2) // right paddle collision
+            {
+                // TODO play a "paddle hit" sound 
+                // TODO use ballMoveRight boolean to change direction
+            }
+
+            #endregion
+
+            #region ball collision with side walls (point scored)
+
+            player = new SoundPlayer(Properties.Resources.score);
+
+            if (ballX < 0)  // TODO ball hits left wall logic
+            {
+                // --- play score sound
+                // --- update player 2 score
+                // --- update player2Label with new score
+                // --- refresh
+
+                // --- use if statement to check to see if player 2 has won the game. If true run 
+                // gameWinScore method. Else call SetParameters method to reset ball position.
+
+            }
+
+            // TODO same as above but this time check for collision with the right wall
+
+            #endregion
+            
+            //refresh the screen, which causes the Form1_Paint method to run
+            this.Refresh();
+        }
+        
+        /// <summary>
+        /// Displays a message for the winner when the game is over and allows the user to either select
+        /// to play again or end the program
+        /// </summary>
+        /// <param name="winner">The player name to be shown as the winner</param>
+        private void GameOver(string winner)
+        {
+            newGameOk = true;
+
+            // TODO create game over logic
+            // --- stop the gameUpdateLoop
+            // --- show a message on the startLabel to indicate a winner
+            // --- pause for two seconds 
+            // --- use the startLabel to ask the user if they want to play again
+
+        }
+
+        private void closeButton_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            //double buffering used to produce flicker free animation
+            this.DoubleBuffered = true;
+
+            //create graphic objects
+            Graphics formGraphics = e.Graphics;
+            SolidBrush drawBrush = new SolidBrush(Color.White);
+
+            // TODO draw paddles using FillRectangle
+
+            // TODO draw ball using FillRectangle
+
+        }
+
+    }
+}
